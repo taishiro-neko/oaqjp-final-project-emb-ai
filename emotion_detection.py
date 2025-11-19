a5c8643cef5cce0e743ac444fd880ca22115d142
Cat Gray <cat@catness.org>
2025-11-19 12:31:21 +0200

    emotion detector works

---
+++ emotion_detection.py
@@ -0,0 +1,23 @@
+import requests 
+import json
+
+def emotion_detector(text_to_analyse):  
+    
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+	formatted_response = json.loads(response.text)
+	#pretty_json = json.dumps(formatted_response, indent=4)
+    #print (pretty_json)
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions["dominant_emotion"] = dominant_emotion
+
+    if response.status_code == 200:
+    	return json.dumps(emotions, indent=4)
+
+    elif response.status_code == 500:
+        return {}
+
